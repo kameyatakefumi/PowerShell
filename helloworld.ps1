3db501bda429a515b77f050b1cc4000c859e519d
kameya <kameya.takefumi@gmail.com>
2016-10-19 22:13:13 +0900

    add variable types

--- helloworld.ps1
+++ helloworld.ps1
@@ -4,3 +4,9 @@ Write-Host "Hello world!"
 # Variable
 $msg = "hello world!!"
 Write-Host $msg
+
+# Variable Types
+[string] $str = "String"
+[int] $int = 999
+Write-Host $str.GetType()
+Write-Host $int.GetType()
